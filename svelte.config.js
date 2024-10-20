df60682ff75279d3c008ae9342c728febd055fad
shubh <shubhvjain@gmail.com>
2024-10-20 14:29:08 +0200

    using netlify now

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-    }),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			edge: false,
+			split: false
+		})
 	}
 };
 
